ec3355f1dde9422c373cf8d39f00c7a1b13100cb
DylanSantiago <2dylansantiago@gmail.com>
2022-12-04 17:57:01 -0500

    app js structure update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Portfolio from './components/pages/portfolio';
+import Resume from './components/pages/resume';
 
 export default function App() {
   return(
     <main>
       <Nav />
       <Me />
-      <Project />
+      <Portfolio />
+      <Resume />
       <Contact />
     </main>
   )
