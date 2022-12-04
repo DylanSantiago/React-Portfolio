294d72d3ae0010e0a524b3ec2471ccebe2169e5a
DylanSantiago <2dylansantiago@gmail.com>
2022-12-04 18:43:44 -0500

    contact page created

---
+++ my-react-app/src/components/pages/contact.js
@@ -0,0 +1,16 @@
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact Me!</h1>
+                <p>
+                   Email: <a href='mailto:2dylansantiago@gmail.com'>Email Me Here</a> 
+                </p>
+                <p>
+                    Github: <a href='https://github.com/DylanSantiago'>Link to Github</a>
+                </p>
+                <p>
+                   LinkedIn: <a href='www.linkedin.com/in/dylan-santiago-bb536820a'>Click Here to Connect</a> 
+                </p>
+        </div>
+    )
+}
