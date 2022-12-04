63c4bd05726829e1a8d0330ea874594dd5f60c25
DylanSantiago <2dylansantiago@gmail.com>
2022-12-04 13:59:15 -0500

    about me filed updated

---
+++ my-react-app/src/components/pages/me.js
@@ -0,0 +1,17 @@
+export default function Me() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            
+            <p> Hello Page Viewer! My Name is Dylan Santiago and I'm a 23 year old aspiring Full Stack Developer.
+            
+            I'm new to coding, and I'm loving everything I'm learning. I'm facing every challenge head on and embracing the difficult 
+            
+            challenges that lie ahead because I know that it will be worth it in the end. Outside of my new cyber life I enjoy being active 
+            
+            in the real world. I enjoy music festivals, playing sports, and spending time with friends and family.
+           
+           </p>
+        </div>
+    )
+}
