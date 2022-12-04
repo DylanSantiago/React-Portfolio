f637595a6015abeabc5331c125087952ee7758cc
DylanSantiago <2dylansantiago@gmail.com>
2022-12-03 21:49:22 -0500

    code added to nav bar

---
+++ my-react-app/src/components/nav.js
@@ -0,0 +1,40 @@
+export default function Navbar({ currentPage, handlePageChange }) {
+    return (
+      <nav className="">
+        <ul className="">
+          <li className="">
+            <a href = "#home"
+               onClick = {() => handlePageChange('Home')}
+               className = {currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+            >
+              About Me
+            </a>
+          </li>
+          <li className="">
+            <a href = "#portfolio"
+               onClick = {() => handlePageChange('Portfolio')}
+               className = {currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="">
+            <a href = "#resume"
+               onClick = {() => handlePageChange('Resume')}
+               className = {currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+            >
+              Resume
+            </a>
+          </li>
+          <li className="">
+            <a href = "#contact"
+               onClick = {() => handlePageChange('Contact')}
+               className = {currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
