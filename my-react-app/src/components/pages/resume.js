cd05d538cc1323c68445aa98700b322868a9b1ce
DylanSantiago <2dylansantiago@gmail.com>
2022-12-04 15:21:04 -0500

    resume updated

---
+++ my-react-app/src/components/pages/resume.js
@@ -0,0 +1,32 @@
+export default function Resume() {
+    return (
+        <div>
+            <h1>Work History</h1>
+                <div>
+                    <h2>March 2022 - Present</h2>
+                        <h4>ETD Clerical Associate, Hackensack Meridian Health</h4>
+                            <p>Responisble for all clerical responsibilities throughout the entire Emergency Trauma Department.
+                            Would notify the correct people in response to codes all while delivering prompt and ensuring patient satisfaction.
+                            </p> 
+                </div>
+                <div>
+                    <h2>June 2018 - February 2022</h2>
+                        <h4>Service Agent, Hackensack Meridian Health</h4>
+                            <p>Primary responisbilites were to ensure prompt car service and handle patients and customers vehicles with the utmost care.</p> 
+                </div>
+            <h1>School History</h1>
+                <div>
+                    <h2>June 2022 - December 2022</h2>
+                        <h4>Rutgers Coding Bootcamp</h4>
+                </div>
+                <div>
+                    <h2>September 2016 - May 2018</h2>
+                        <h4>Associates Degree in General Studies, Bergen Community College</h4>
+                </div>
+                <div>
+                    <h2>September 2012 - June 2016</h2>
+                        <h4>High School Degree, Garfield High School</h4>
+                </div>
+        </div>
+    )
+}
