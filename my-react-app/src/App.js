ff5339be4890d05b8bb79e7341aaf41b0f405762
DylanSantiago <2dylansantiago@gmail.com>
2022-12-04 20:52:28 -0500

    added dependencies and imported react

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,15 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Portfolio from './components/pages/portfolio';
 import Resume from './components/pages/resume';
+import Navbar from './components/nav';
+import Contact from './components/pages/contact';
+import Me from './components/pages/me';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
-  return(
-    <main>
+  return (
+    <Router>
       <Nav />
       <Me />
       <Portfolio />
       <Resume />
       <Contact />
-    </main>
+    </Router>
   )
 }
--- my-react-app/src/components/pages/me.js
+++ my-react-app/src/components/pages/me.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Me() {
     return (
         <div>
