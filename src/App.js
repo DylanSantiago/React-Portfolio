af77ad2ebc4065f72356ec22f96ee9d7b1ba33e3
DylanSantiago <2dylansantiago@gmail.com>
2022-12-05 00:43:14 -0500

    found issue that was preventing page from rendering and chaanged code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import Portfolio from './components/pages/portfolio';
 import Resume from './components/pages/resume';
 import Navbar from './components/nav';
 import Contact from './components/pages/contact';
 import Me from './components/pages/me';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import React, { useState } from 'react';
 export default function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Me />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+        return <Resume />;
+    }
+    return <Contact />;
+  };
+  const handlePageChange = (page) => setCurrentPage(page);
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route
-              path="/"
-              element={<Me />}
-            />
-             <Route
-              path="/portfolio"
-              element={<Portfolio />}
-            />
-             <Route
-              path="/resume"
-              element={<Resume />}
-            />
-             <Route
-              path="/contact"
-              element={<Contact />}
-            />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <div>
+    <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+    {renderPage()}
+  </div>
   )
 }
