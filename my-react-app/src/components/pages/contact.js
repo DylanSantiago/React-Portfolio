db1af5845e072607b8be796c1708e3ce3644d336
DylanSantiago <2dylansantiago@gmail.com>
2022-12-04 19:36:55 -0500

    contact page updated

--- my-react-app/src/components/pages/contact.js
+++ my-react-app/src/components/pages/contact.js
@@ -3,13 +3,13 @@ export default function Contact() {
         <div>
             <h1>Contact Me!</h1>
                 <p>
-                   Email: <a href='mailto:2dylansantiago@gmail.com'>Email Me Here</a> 
+                   Email: <a href='mailto: 2dylansantiago@gmail.com'>Email Me Here</a> 
                 </p>
                 <p>
                     Github: <a href='https://github.com/DylanSantiago'>Link to Github</a>
                 </p>
                 <p>
-                   LinkedIn: <a href='www.linkedin.com/in/dylan-santiago-bb536820a'>Click Here to Connect</a> 
+                   Twitter: <a href='https://twitter.com/dylannnsant'>Click Here to Connect</a> 
                 </p>
         </div>
     )
